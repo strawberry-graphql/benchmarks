8f03609db12f5a203607673c4f7be0a6eed123b7
Patrick Arminio <patrick.arminio@gmail.com>
2022-12-31 17:50:29 +0000

    Run test on mac studio

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -11,22 +11,13 @@ many_fields_query_with_directives = (
 
 class ExecuteSync:
     def time_execute(self):
-        for _ in range(100):
-            schema.execute_sync(basic_query)
-
-    time_execute.repeat = 1
+        schema.execute_sync(basic_query)
 
     def time_execute_with_many_fields(self):
         schema.execute_sync(many_fields_query)
 
-    time_execute_with_many_fields.repeat = 10
-
-    def mem_execute_with_many_fields(self):
+    def peakmem_execute_with_many_fields(self):
         schema.execute_sync(many_fields_query)
-
-    mem_execute_with_many_fields.repeat = 10
 
     def time_execute_with_many_fields_and_directives(self):
         schema_with_directives.execute_sync(many_fields_query_with_directives)
-
-    time_execute_with_many_fields_and_directives.repeat = 10
